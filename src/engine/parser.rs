d5936e14241c2015b05df24c424c1c91687303ea
shimatar0 <shimataro0579@gmail.com>
2023-06-03 21:36:39 +0900

    Add Or operator

--- src/engine/parser.rs
+++ src/engine/parser.rs
@@ -56,6 +56,19 @@ enum PSQ {
     Question,
 }
 
+fn fold_or(mut seq_or: Vec<AST>) -> Option<AST> {
+    if seq_or.len() > 1 {
+        let mut ast = seq_or.pop().unwrap();
+        seq_or.reverse();
+        for s in seq_or {
+            ast = AST::Or(Box::new(s), Box::new(ast));
+        }
+        Some(ast)
+    } else {
+        seq_or.pop()
+    }
+}
+
 mod test {
     #[test]
     fn parse_escape() {
@@ -71,5 +84,33 @@ mod test {
         assert_eq!(parse_escape(0, '*'), Ok(AST::Char('*')));
         assert_eq!(parse_escape(0, '_'), Ok(AST::Char('_')));
         assert_eq!(parse_escape(0, 'a'), Err(ParseError::InvalidEscape(0, 'a')));
+    }
+
+    #[test]
+    fn fold_or() {
+        use super::fold_or;
+        use super::AST;
+
+        assert_eq!(fold_or(vec![]), None);
+        assert_eq!(fold_or(vec![AST::Char('a')]), Some(AST::Char('a')));
+        assert_eq!(
+            fold_or(vec![AST::Char('a'), AST::Char('b')]),
+            Some(AST::Or(Box::new(AST::Char('a')), Box::new(AST::Char('b'))))
+        );
+        assert_eq!(
+            fold_or(vec![
+                AST::Char('a'),
+                AST::Char('b'),
+                AST::Char('c'),
+                AST::Char('d')
+            ]),
+            Some(AST::Or(
+                Box::new(AST::Char('a')),
+                Box::new(AST::Or(
+                    Box::new(AST::Char('b')),
+                    Box::new(AST::Or(Box::new(AST::Char('c')), Box::new(AST::Char('d'))))
+                ))
+            ))
+        );
     }
 }
